f4d6ec174f94c9a1c67584298d77a5278cbc4b3f
ViniTeider <vini469teider@gmail.com>
2025-01-06 22:22:42 -0300

    feat: :sparkles: added A LOT of stuff
    added funny texts on the background, player control using joystick and AWSD keys, player animations for running and jumping and lastly added sprite directions

--- scripts/killzone.gd
+++ scripts/killzone.gd
@@ -2,12 +2,17 @@ extends Area2D
 
 @onready var timer: Timer = $Timer
 
+# When the player colides with this
 func _on_body_entered(body: Node2D) -> void:
-	# When the player colides with this
 	print("You died")
+	
+	Engine.time_scale = 0.5 # Slows down time 
+	body.get_node("CollisionShape2D").queue_free() # Removes the Collision from the body that collided (player)
+	
 	timer.start() # Calls a timer
 
 
 func _on_timer_timeout() -> void:
+	Engine.time_scale = 1
 	# Restarts the scene
 	get_tree().reload_current_scene()
--- scripts/player.gd
+++ scripts/player.gd
@@ -1,5 +1,6 @@
 extends CharacterBody2D
 
+@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
 
 const SPEED = 130.0
 const JUMP_VELOCITY = -300.0
@@ -9,14 +10,14 @@ var jump_number = 0
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
-		if Input.is_action_just_pressed("ui_accept") and jump_number < 1:
+		if Input.is_action_just_pressed("jump") and jump_number < 1:
 			velocity.y = JUMP_VELOCITY
 			jump_number += 1
 		else:
 			velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 		jump_number += 1
 		
@@ -24,9 +25,26 @@ func _physics_process(delta: float) -> void:
 	if is_on_floor():
 		jump_number = 0
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
+	# Gets the input direction (-1, 0, 1)
+	var direction := Input.get_axis("move_left", "move_right")
+	
+	# Flip the sprite
+	if direction > 0:
+		animated_sprite.flip_h = false
+	elif direction < 0:
+		animated_sprite.flip_h = true
+		
+	# Play animations
+	if is_on_floor():
+		if direction == 0:
+			animated_sprite.play("idle")
+		else:
+			animated_sprite.play("run")
+	else:
+		animated_sprite.play("jump")
+	
+	
+	# Aply the movement
 	if direction:
 		velocity.x = direction * SPEED
 	else:
